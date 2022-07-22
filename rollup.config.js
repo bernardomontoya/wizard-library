44205360a2a8531cc189c0dd6d066a9bbf77dee8
Bernardo Montoya <ber.montoya12@gmail.com>
2022-07-21 21:39:54 -0600

    feat: add Postcss plugin for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import externals from 'rollup-plugin-node-externals';
+import postcss from 'rollup-plugin-postcss';
 import del from 'rollup-plugin-delete';
 import pkg from './package.json';
 
@@ -19,6 +20,16 @@ export default [
         babelHelpers: 'runtime',
         exclude: '**/node_modules/**',
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      }),
+      postcss({
+        minimize: true,
+        modules: true,
+        use: {
+          sass: null,
+          stylus: null,
+          less: { javascriptEnabled: true },
+        },
+        extract: true,
       }),
     ],
     output: [
